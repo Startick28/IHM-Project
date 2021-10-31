e6928cc9313b6ebef5a731749d14adc52f2fe2d0
Timothé Louzier <louziert@gmail.com>
2021-10-31 23:09:26 +0100

    MovingPlatforms begining

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -10,6 +10,9 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] private float waitingTime;
 
     int currentCheckpoint= 0;
+    float currentVelocityX;
+    float currentVelocityY;
+    Vector3 lastPosition;
 
     void Start()
     {
@@ -24,13 +27,16 @@ public class MovingPlatform : MonoBehaviour
             Vector3 endPosition;
             if (currentCheckpoint + 1 < listOfCheckpoints.Count) endPosition = listOfCheckpoints[currentCheckpoint+1].position;
             else endPosition = listOfCheckpoints[0].position;
-
+            
             float duration = Vector3.Distance(startPosition,endPosition) / speed;
             for (float time = 0; time < duration; time+=Time.deltaTime)
             {
+                lastPosition = transform.position;
                 float t = time/duration;
                 t= t*t*(3f-2f*t);
                 transform.position = Vector3.Lerp(startPosition, endPosition, t);
+                currentVelocityX = (transform.position.x - lastPosition.x) / Time.deltaTime;
+                currentVelocityY = (transform.position.y - lastPosition.y) / Time.deltaTime;
                 yield return null;
             }            
             transform.position = endPosition;
@@ -42,5 +48,14 @@ public class MovingPlatform : MonoBehaviour
             }
             yield return new WaitForSeconds(waitingTime);
         }
+    }
+
+    public float GetVelocityX()
+    {
+        return currentVelocityX;
+    }
+    public float GetVelocityY()
+    {
+        return currentVelocityY;
     }
 }
